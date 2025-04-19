8041fec222ae3cadf4395c42919b1fd59db4ee0f
shrenikm <shrenik95@gmail.com>
2025-04-18 23:17:36 -0700

    hmm

--- atium/implementations/unito/src/start.py
+++ atium/implementations/unito/src/start.py
@@ -1,1 +1,23 @@
-import pydrake
+from pydrake.solvers import MathematicalProgram, Solve
+
+prog = MathematicalProgram()
+
+
+x = prog.NewContinuousVariables(1, "x")
+y = prog.NewContinuousVariables(1, "y")
+c = prog.AddConstraint(x[0] + y[0] == 1)
+prog.AddCost(x[0] ** 2 + y[0] ** 2)
+print(prog)
+
+def f(x):
+    print(prog.EvalBinding(c, [x[0], x[1]]))
+
+prog.AddVisualizationCallback(f, [x, y])
+
+
+res = Solve(prog, [0, 0])
+print(res.is_success())
+print(res.GetSolution(x))
+print(res.GetSolution(y))
+print(res.get_solver_id().name())
+print(res.get_solution_result())
