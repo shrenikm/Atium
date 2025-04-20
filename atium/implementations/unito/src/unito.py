432cd57c0e56af58837c19e1f2afc2e8285f7bca
shrenikm <shrenik95@gmail.com>
2025-04-19 18:09:06 -0700

    fix

--- atium/implementations/unito/src/unito.py
+++ atium/implementations/unito/src/unito.py
@@ -4,6 +4,8 @@ See: https://arxiv.org/abs/2409.07924
 """
 
 import attr
+import numpy as np
+from pydrake.solvers import MathematicalProgram
 
 
 @attr.frozen
@@ -21,6 +23,27 @@ class UnitoParams:
     n: int
 
 
-@attr.s
+@attr.define
 class Unito:
     params: UnitoParams
+
+    # Optimization variables
+    _prog: MathematicalProgram = attr.ib(init=False)
+    _var_c: np.ndarray = attr.ib(init=False)
+    _var_t: np.ndarray = attr.ib(init=False)
+
+    @_prog.default
+    def _init_prog(self):
+        return MathematicalProgram()
+
+    @_var_c.default
+    def _init_var_c(self):
+        return self._prog.NewContinuousVariables(2 * self.params.h * self.params.M, 2, "c")
+
+    @_var_t.default
+    def _init_var_t(self):
+        return self._prog.NewContinuousVariables(self.params.M, 1, "t")
+
+
+params = UnitoParams(h=2, M=3, n=4)
+unito = Unito(params)
