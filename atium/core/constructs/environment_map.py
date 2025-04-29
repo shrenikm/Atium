6b9d4475a2151b4478b3d5fb3d91b7edce90b945
shrenikm <shrenik95@gmail.com>
2025-04-28 23:42:23 -0700

    emap done'

--- atium/core/constructs/environment_map.py
+++ atium/core/constructs/environment_map.py
@@ -28,6 +28,14 @@ class EnvironmentMap2D:
     )
     resolution: float = attr.ib(validator=AttrsValidators.scalar_bounding_box_validator(min_value=0.0, inclusive=False))
 
+    @classmethod
+    def from_empty(cls, size_xy: SizeXY, resolution: float) -> Self:
+        size_px = (int(size_xy[1] / resolution), int(size_xy[0] / resolution))
+        return cls(
+            array=np.zeros(size_px, dtype=np.uint8),
+            resolution=resolution,
+        )
+
     @cached_property
     def size_px(self) -> Shape2D:
         return self.array.shape
@@ -36,21 +44,13 @@ class EnvironmentMap2D:
     def size_xy(self) -> SizeXY:
         return (self.size_px[1] * self.resolution, self.size_px[0] * self.resolution)
 
-    @classmethod
-    def from_empty(cls, size_xy: SizeXY, resolution: float) -> Self:
-        size_px = (int(size_xy[1] // resolution), int(size_xy[0] // resolution))
-        return cls(
-            array=np.zeros(size_px, dtype=np.uint8),
-            resolution=resolution,
-        )
-
     def xy_to_px(self, xy: tuple[float, float]) -> tuple[int, int]:
         """
         Convert a tuple (can be size, coordinates, etc) in meters to a tuple in pixels.
         """
         return (
-            int(xy[0] // self.resolution),
-            int(xy[1] // self.resolution),
+            int(xy[0] / self.resolution),
+            int(xy[1] / self.resolution),
         )
 
     def px_to_xy(self, px: tuple[int, int]) -> tuple[float, float]:
@@ -67,11 +67,11 @@ class EnvironmentMap2D:
         Convert a pixel coordinate to a cv2 coordinate.
         The cv2 coordinate system has the origin at the top-left corner of the image.
         """
-        return (position_px[0], self.size_px[1] - position_px[1])
+        return (position_px[0], self.size_px[0] - position_px[1])
 
     def add_rectangular_obstacle(
         self,
-        position_xy: CoordinateXY,
+        center_xy: CoordinateXY,
         size_xy: SizeXY,
         label: EnvironmentLabels,
         thickness: int = cv2.FILLED,
@@ -81,17 +81,17 @@ class EnvironmentMap2D:
         The rectangle is defined by its center and size.
         """
         # Convert position to pixel coordinates
-        position_px = self.xy_to_px(xy=position_xy)
+        center_px = self.xy_to_px(xy=center_xy)
         size_px = self.xy_to_px(xy=size_xy)
 
         # Calculate the top-left and bottom-right corners of the rectangle
-        top_left = (position_px[0] - size_px[0] // 2, position_px[1] - size_px[1] // 2)
-        bottom_right = (position_px[0] + size_px[0] // 2, position_px[1] + size_px[1] // 2)
+        top_left = (center_px[0] - size_px[0] // 2, center_px[1] - size_px[1] // 2)
+        bottom_right = (center_px[0] + size_px[0] // 2, center_px[1] + size_px[1] // 2)
 
         cv2.rectangle(
             self.array,
             self.get_cv2_coordinates(top_left),
             self.get_cv2_coordinates(bottom_right),
-            color=label.value,
+            color=label,
             thickness=thickness,
         )
--- atium/core/constructs/tests/test_environment_map.py
+++ atium/core/constructs/tests/test_environment_map.py
@@ -1,13 +1,15 @@
+import cv2
 import numpy as np
 import pytest
 
-from atium.core.constructs.environment_map import EnvironmentMap2D
+from atium.core.constructs.environment_map import EnvironmentLabels, EnvironmentMap2D
 
 
 @pytest.fixture(scope="function")
 def emap2d() -> EnvironmentMap2D:
     return EnvironmentMap2D(
-        array=np.zeros((100, 100), dtype=np.uint8),
+        # Asymmetric array to catch bugs.
+        array=np.zeros((100, 200), dtype=np.uint8),
         resolution=0.1,
     )
 
@@ -37,6 +39,70 @@ def test_emap2d_invalid() -> None:
             array=np.zeros((100, 100), dtype=np.int32),
             resolution=0.1,
         )
+
+
+def test_emap2d_from_empty() -> None:
+    emap2d = EnvironmentMap2D.from_empty(
+        size_xy=(20.0, 10.0),
+        resolution=0.1,
+    )
+    np.testing.assert_allclose(
+        emap2d.array,
+        np.zeros((100, 200), dtype=np.uint8),
+    )
+
+
+def test_emap2d_size_px(emap2d: EnvironmentMap2D) -> None:
+    assert emap2d.size_px == (100, 200)
+
+
+def test_emap2d_size_xy(emap2d: EnvironmentMap2D) -> None:
+    assert emap2d.size_xy == (20.0, 10.0)
+
+
+def test_emap2d_xy_to_px(emap2d: EnvironmentMap2D) -> None:
+    # Test the conversion from world coordinates to pixel coordinates.
+    x, y = 1.0, 0.5
+    px_x, px_y = emap2d.xy_to_px((x, y))
+    assert px_x == 10
+    assert px_y == 5
+
+
+def test_emap2d_px_to_xy(emap2d: EnvironmentMap2D) -> None:
+    # Test the conversion from pixel coordinates to world coordinates.
+    px_x, px_y = 10, 5
+    x, y = emap2d.px_to_xy((px_x, px_y))
+    np.testing.assert_allclose(x, 1.0, atol=1e-6)
+    np.testing.assert_allclose(y, 0.5, atol=1e-6)
+
+
+def test_emap2d_get_cv2_coordinates(emap2d: EnvironmentMap2D) -> None:
+    # Test the conversion from world coordinates to OpenCV coordinates.
+    px_x, px_y = 24, 7
+    cv2_x, cv2_y = emap2d.get_cv2_coordinates((px_x, px_y))
+    assert cv2_x == 24
+    assert cv2_y == 93
+
+
+@pytest.mark.parametrize("thickness", [1, cv2.FILLED])
+def test_emap2d_add_rectangular_obstacle(
+    emap2d: EnvironmentMap2D,
+    thickness: int,
+    debug: bool = True,
+) -> None:
+    # Test adding a rectangular obstacle to the environment map.
+    emap2d.add_rectangular_obstacle(
+        center_xy=(10.0, 5.0),
+        size_xy=(1.0, 3.0),
+        label=EnvironmentLabels.STATIC_OBSTACLE,
+        thickness=thickness,
+    )
+    if debug:
+        cv2.imshow(
+            "EnvironmentMap2D",
+            emap2d.array,
+        )
+        cv2.waitKey(0)
 
 
 if __name__ == "__main__":
