67e7193c53aa5e485aa949e993688fbb839d20f4
shrenikm <shrenik95@gmail.com>
2025-05-02 23:06:14 -0700

    polygon stuff

--- atium/core/utils/geometry_utils.py
+++ atium/core/utils/geometry_utils.py
@@ -1,8 +1,49 @@
 import numpy as np
 
-from atium.core.utils.custom_types import AngleOrAnglesRad
+from atium.core.utils.custom_types import AngleOrAnglesRad, PolygonXYArray, SizeXY
 
 
 def normalize_angle(angles: AngleOrAnglesRad) -> AngleOrAnglesRad:
     # https://stackoverflow.com/questions/15927755/opposite-of-numpy-unwrap
     return (np.array(angles) + np.pi) % (2 * np.pi) - np.pi
+
+
+def construct_rectangle_polygon(
+    size_xy: SizeXY,
+) -> PolygonXYArray:
+    """
+    Constructs a rectangle polygon with the given size.
+    The rectangle is centered at the origin and aligned with the x-y axes.
+    """
+    width, height = size_xy
+    return np.array(
+        [
+            [-width / 2, -height / 2],
+            [width / 2, -height / 2],
+            [width / 2, height / 2],
+            [-width / 2, height / 2],
+        ],
+        dtype=np.float64,
+    )
+
+
+def densify_polygon(polygon: PolygonXYArray, distance_between_points: float) -> PolygonXYArray:
+    """
+    Densify a polygon represented by Nx2 corner points by adding points along
+    each edge at roughly `distance_between_points` intervals.
+    """
+    segments_start = polygon
+    segments_end = np.roll(polygon, -1, axis=0)  # shift to get the next point, wrapping around
+
+    edge_vectors = segments_end - segments_start
+    edge_lengths = np.linalg.norm(edge_vectors, axis=1)
+
+    all_points = []
+
+    for start, vec, length in zip(segments_start, edge_vectors, edge_lengths, strict=True):
+        num_segments = max(int(np.floor(length / distance_between_points)), 1)
+        t_values = np.linspace(0, 1, num_segments, endpoint=False)
+        points = start + np.outer(t_values, vec)
+        all_points.append(points)
+
+    return np.vstack(all_points)
---
+++ atium/core/utils/tests/test_geometry_utils.py
@@ -0,0 +1,86 @@
+import numpy as np
+import pytest
+
+from atium.core.utils.geometry_utils import construct_rectangle_polygon, densify_polygon, normalize_angle
+
+
+def test_normalize_angle() -> None:
+    input_and_expected_angles = [
+        (0.0, 0.0),
+        (1.0, 1.0),
+        (-1.0, -1.0),
+        (np.pi - 0.1, np.pi - 0.1),
+        (-np.pi + 0.1, -np.pi + 0.1),
+        (2 * np.pi, 0.0),
+        (-2 * np.pi, 0.0),
+        (5 * np.pi / 2.0, np.pi / 2.0),
+        (-5 * np.pi / 2.0, -np.pi / 2.0),
+    ]
+    for input_angle, expected_angle in input_and_expected_angles:
+        normalized_angle = normalize_angle(input_angle)
+        np.testing.assert_allclose(
+            normalized_angle,
+            expected_angle,
+            atol=1e-12,
+        )
+
+
+def test_construct_rectangle_polygon() -> None:
+    polygon = construct_rectangle_polygon(
+        size_xy=(1.0, 2.0),
+    )
+    expected_polygon = np.array(
+        [
+            [-0.5, -1.0],
+            [0.5, -1.0],
+            [0.5, 1.0],
+            [-0.5, 1.0],
+        ],
+        dtype=np.float64,
+    )
+    np.testing.assert_allclose(
+        polygon,
+        expected_polygon,
+        atol=1e-12,
+    )
+
+
+def test_densify_polygon() -> None:
+    polygon = construct_rectangle_polygon(
+        size_xy=(8.0, 6.0),
+    )
+    dense_polygon = densify_polygon(
+        polygon=polygon,
+        distance_between_points=1.0,
+    )
+    expected_polygon = np.array(
+        [
+            [-4.0, -3.0],
+            [-3.0, -3.0],
+            [-2.0, -3.0],
+            [-1.0, -3.0],
+            [0.0, -3.0],
+            [1.0, -3.0],
+            [2.0, -3.0],
+            [3.0, -3.0],
+            [4.0, -3.0],
+            [4.0, 3.0],
+            [3.0, 3.0],
+            [2.0, 3.0],
+            [1.0, 3.0],
+            [0.0, 3.0],
+            [-1.0, 3.0],
+            [-2.0, 3.0],
+            [-3.0, 3.0],
+        ],
+        dtype=np.float64,
+    )
+    np.testing.assert_allclose(
+        dense_polygon,
+        expected_polygon,
+        atol=1e-12,
+    )
+
+
+if __name__ == "__main__":
+    pytest.main(["-s", "-v", __file__])
