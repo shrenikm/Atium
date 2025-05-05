6d029db5c8c2aa9ae65a62122e8190ccff9d9412
shrenikm <shrenik95@gmail.com>
2025-05-04 22:33:52 -0700

    fix

--- atium/experiments/runito/src/runito_utils.py
+++ atium/experiments/runito/src/runito_utils.py
@@ -1,7 +1,7 @@
 import attr
 
-from atium.core.constructs.environment_map import EnvironmentMap2D
 from atium.core.definitions.concrete_states import Pose2D, Velocity2D
+from atium.core.definitions.environment_map import EnvironmentMap2D
 from atium.core.utils.attrs_utils import AttrsValidators
 from atium.core.utils.custom_types import (
     NpMatrix22f64,
